33a90965e42e53dcfb7b573cec25327384a9d17d
shashilaheshan <ebayshashila@gmail.com>
2018-11-16 12:12:40 +0530

    Designed ui and Ts files to get json data from api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { HttpModule } from '@angular/http';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -13,6 +13,7 @@ import { HomePage } from '../pages/home/home';
     HomePage
   ],
   imports: [
+    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  current:any=[];
+  location:any =[];
+city:any="";
+url:any="";
+name:any="";
+region:any="";
+country:any="";
+lat:any="";
+lon:any="";
+time:any="";
+temp_c:any="";
+temp_f:any="";
+pressure:any="";
+humidity:any="";
+  constructor(public navCtrl: NavController,public http:Http) {
 
   }
+  ionViewDidLoad(){
 
+    this.GetData();
+  }
+        GetData(){
+          if(this.city){
+            let url="http://api.apixu.com/v1/current.json?key=c612fb584fee4f7ea9f43145172512&q=";
+            this.http.get(url+this.city).map(res => res.json()).subscribe(data => {
+              
+              this.current=data.current;
+              this.location=data.location;
+              // console.log(data);
+              console.log(this.current.condition.icon);
+              this.url=this.current.condition.icon;
+              this.name=this.location.name;
+              this.region=this.location.region;
+              this.country=this.location.country;
+              this.lat=this.location.lat;
+              this.lon=this.location.lon;
+              this.time=this.location.localtime;
+              this.temp_c=this.current.temp_c;
+              this.temp_f=this.current.temp_f;
+              this.pressure=this.current.pressure_in;
+              this.humidity=this.current.humidity;
+           
+          });
+            
+          }
+         
+           
+
+        }
 }
